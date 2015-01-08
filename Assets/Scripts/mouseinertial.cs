48267f4d5e01009cabd04c590db6515261c95688
Sergio <sergi79@gmail.com>
2015-01-08 13:24:46 +0100

    rayote de la muerte

--- Assets/Scripts/mouseinertial.cs
+++ Assets/Scripts/mouseinertial.cs
@@ -7,7 +7,8 @@ public class mouseinertial : MonoBehaviour
 	//
 	public float fuerza=300;
 	//
-	private GameObject[] _clonelinerender;
+	public float speed =1.0f;
+	private Vector3 range=new Vector3 (0.3f, 0.3f, 0.3f); 
 	private Vector3 _screenPoint;
 	private Vector3 _offset;
 	private Vector3 _curScreenPoint;
@@ -15,57 +16,28 @@ public class mouseinertial : MonoBehaviour
 	private Vector3 _velocity;
 	private bool _pressmouse;
 	private float _time = 0.0f;
-	private Vector3 _direccion,_temp;
-	private float _desviationy=0.0f;
+	private Vector3 _temp;
+	Perlin _noise;
 	private bool bajando=false;
-	private int renderline;
-	private GameObject hand;
 
-
-	void Start()
-	{
-		_clonelinerender = new GameObject[20];
-		renderline=0;
-	}
 
 	void FixedUpdate()	{
 		if (_pressmouse) {
 			//aqui vamos a hacer un movimiento en el eje de las y para simular que esta flotando
 
-			if (bajando)_desviationy=_desviationy-0.01f;
-			else _desviationy=_desviationy+0.01f;
-
-			//if (_desviationy>0.25f) {bajando=true;}
-			//if (_desviationy<=-0.25f) {bajando=false;}
-
-			if (_desviationy>0.3f) {bajando=true;}
-			if (_desviationy<=-0.3f) {bajando=false;}
+			if (_noise == null) _noise = new Perlin ();
 			/////
 			Vector3 _prevPosition = _curPosition;
 			_curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
 			_curPosition = Camera.main.ScreenToWorldPoint(_curScreenPoint) + _offset;
 			_velocity = _curPosition - _prevPosition;
-			_curPosition.y=_curPosition.y+_desviationy;
+
+			_curPosition = _curPosition + Vector3.Scale(SmoothRandom.GetVector3(speed), range);
 			transform.position = _curPosition;
+
 			rigidbody.velocity = Vector3.zero;	
 			//dirigir el haz de particulas al objeto.
 			_temp=Camera.main.transform.position;
-			_direccion=_curPosition-_temp;
-
-			//mientras este pulsado creamos diferentes rayos
-			Destroy(_clonelinerender[renderline]); //destruimos el rayo anterior
-			/// creamos el nuevo rayo.
-			Object linerender = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/linerender.prefab", typeof(GameObject));
-			_clonelinerender[renderline] = (GameObject)Instantiate(linerender);		
-			linerenderscript script = _clonelinerender[renderline].GetComponent("linerenderscript") as linerenderscript;
-
-			hand = GameObject.Find("Beta:RightHand");
-			script.origin=hand.transform;
-			
-			script.destination=gameObject.transform;
-
-			if (renderline<_clonelinerender.Length-1) renderline++;
-			else renderline=0;
 		}
 
 	}
@@ -75,12 +47,10 @@ public class mouseinertial : MonoBehaviour
 		if (Input.GetMouseButtonDown (1)) {
 					if (_pressmouse) _pressmouse = false;
 					rigidbody.AddForce(Camera.main.transform.forward * 500);
-					deleterays();
 				}
 		if (Input.GetMouseButtonDown (2)) {
 					if (_pressmouse)_pressmouse = false;
 					rigidbody.AddForce(-Camera.main.transform.forward * 500);
-					deleterays();
 				}
 	}
 
@@ -94,25 +64,25 @@ public class mouseinertial : MonoBehaviour
 						_offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z));
 						Screen.showCursor = false;
 						rigidbody.velocity = Vector3.zero;
-						rigidbody.AddTorque (new Vector3 (10, 10, 0) * fuerza); //rotacion al cogerlo en vueloç
-					
+						//rigidbody.AddTorque (new Vector3 (10, 10, 0) * fuerza); //rotacion al cogerlo en vueloç
+						//emitimos particulas
+						
+						GameObject hand = GameObject.Find("Beta:RightHand");
+						LightningBolt script = hand.GetComponent("LightningBolt") as LightningBolt;
+			            script.target=gameObject.transform;
+						script = hand.GetComponent("LightningBolt") as LightningBolt;
+						//hand.particleEmitter.emit = true;
+						////
 				}
 		else {
 				Screen.showCursor = true;
 				rigidbody.AddForce(_velocity * fuerza); //fuerza de inercia.
+				GameObject hand = GameObject.Find("Beta:RightHand");
+				//hand.particleEmitter.emit = false;
+				LightningBolt script = hand.GetComponent("LightningBolt") as LightningBolt;
+				script.target=null;
 				_pressmouse = false;
-				deleterays();
 			}
 
-	}
-	void deleterays()
-	{
-		//apagar las particulas
-		int i=0;
-		while (i < _clonelinerender.Length) 
-		{
-			Destroy(_clonelinerender[i]);
-			i++;
-		}
 	}
 }
---
+++ Assets/Smothmove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smothmove : MonoBehaviour {
+
+	public float speed =1.0f;
+	private Vector3 range=new Vector3 (1.0f, 1.0f, 1.0f); 
+	private Perlin noise;
+	private Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+		noise = new Perlin ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = position + Vector3.Scale(SmoothRandom.GetVector3(speed), range);
+	}
+}
