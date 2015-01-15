02fbd3310ae0ef825ee6f2f7a3f71b135f1a92eb
Sergio <sergi79@gmail.com>
2015-01-15 11:55:27 +0100

    iluminar los objetos

--- Assets/Scripts/abiltycontroller.cs
+++ Assets/Scripts/abiltycontroller.cs
@@ -50,13 +50,33 @@ public class abiltycontroller : MonoBehaviour {
 
 			if (Input.GetMouseButton(0)) 
 			{ 
-				script.energy=script.energy+1000*Time.deltaTime;
+				script.energy=script.energy+20*Time.deltaTime;
 			}
 
 			if (Input.GetMouseButtonUp (1)) 
 			{ 
 				GameObject Beta = GameObject.Find("Beta");
 				_beamobject.rigidbody.AddForce(Beta.transform.forward * script.energy);
+
+			   //miramos si el raton en este momento hace hit sobre algun objeto, si es asi lanzamos en esa direccion
+				Ray ray =Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if(Physics.Raycast(ray, out hit, 100))
+				{
+
+					Vector3 direction=hit.point-_beamobject.transform.position;
+					//direction=direction.normalize();
+					if (_beamobject.transform!=hit.transform)
+					_beamobject.rigidbody.AddForce(direction.normalized*script.energy,ForceMode.Impulse);
+
+				}
+				else//si no hace hit marcamos una distancia de 100 y apuntamos alli lanzando en esa direccion.
+				{
+					_beamobject.rigidbody.AddForce(ray.direction.normalized * script.energy,ForceMode.Impulse);
+				}
+
+				
+
 				script.energy=50; //esto no es necesario pero bueno.
 				_key_press = "f";
 			}
@@ -86,8 +106,12 @@ public class abiltycontroller : MonoBehaviour {
 			}
 			break;
 		case "f":
+			//vamos a iluminar todos los objetos a mi alrededor
+			Iluminate (Color.blue,"Beamer");
+			Iluminate (Color.black,"Tractor");
 			if (!_beam){
 				if (Input.GetMouseButtonDown (0)) {
+					Iluminate (Color.black,"Beamer");
 					Ray ray =Camera.main.ScreenPointToRay(Input.mousePosition);
 					RaycastHit hit;
 					if(Physics.Raycast(ray, out hit, 100))//,(1<<LayerMask.NameToLayer("Beamer"))))
@@ -111,6 +135,8 @@ public class abiltycontroller : MonoBehaviour {
 			else
 			{
 				//retiramos la inercia y el rayo del objecto
+				Iluminate (Color.black,"Beamer");
+				Iluminate (Color.black,"Tractor");
 				Destroy (_beamobject.GetComponent("Intertia"));
 				GameObject hand = GameObject.Find("Beta:RightHand");
 				LightningBolt script = hand.GetComponent("LightningBolt") as LightningBolt;
@@ -119,11 +145,15 @@ public class abiltycontroller : MonoBehaviour {
 				_lateral=0;
 				_horizontal=0;
 				_key_press = "";
+
 			}
 			break;
 		case "g":
+			Iluminate (Color.red,"Tractor");
+			Iluminate (Color.black,"Beamer");
 			if (!_tractor){
 				if (Input.GetMouseButtonDown (0)) {
+					Iluminate (Color.black,"Tractor");
 					Ray ray =Camera.main.ScreenPointToRay(Input.mousePosition);
 					RaycastHit hit;
 					if(Physics.Raycast(ray, out hit, 100))
@@ -131,7 +161,6 @@ public class abiltycontroller : MonoBehaviour {
 						if (hit.collider.gameObject.tag == "Tractor"){
 							if(hit.collider.gameObject.GetComponent<InverseInertia>() == null)
 							{
-
 								GameObject Beta = GameObject.Find("Beta");
 								Beta.AddComponent("InverseInertia");
 								InverseInertia script = Beta.GetComponent("InverseInertia") as InverseInertia;
@@ -146,7 +175,6 @@ public class abiltycontroller : MonoBehaviour {
 								//test.position=hit.point;
 								script2.target=hit.point;
 								script2.active=true;
-							
 							}
 						}
 					}
@@ -155,6 +183,8 @@ public class abiltycontroller : MonoBehaviour {
 			}
 			else
 			{
+				Iluminate (Color.black,"Tractor");
+				Iluminate (Color.black,"Beamer");
 				_tractor=false;
 				GameObject Beta = GameObject.Find("Beta");
 				InverseInertia script = Beta.GetComponent("InverseInertia") as InverseInertia;
@@ -162,6 +192,7 @@ public class abiltycontroller : MonoBehaviour {
 				GameObject hand = GameObject.Find("Beta:LeftHand");
 				LightningBolt script2 = hand.GetComponent("LightningBolt") as LightningBolt;
 				script2.active=false;
+				_key_press = "";
 			}
 		break;
 		default:
@@ -178,5 +209,16 @@ public class abiltycontroller : MonoBehaviour {
 			string temp=script.energy.ToString();
 			GUI.Label (new Rect (10, 10, 150, 20), "Push Force: "+temp);
 				}
+	}
+	void Iluminate(Color c, string tag)
+	{
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere (explosionPos, 100);
+		foreach (Collider hit in colliders) {
+			if ((hit) && (hit.rigidbody)&&(hit.collider.gameObject.tag == tag)) {
+				hit.rigidbody.renderer.material.SetColor("_EmisColor", c);
+			}
+		}
+
 	}
 }
